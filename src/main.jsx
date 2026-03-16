9c67eae3909577c316982bccab88b712121c828f
pizon8113 <pizon8113@gmail.com>
2026-03-16 23:51:10 +0900

    Add: create missing main final component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // .jsx를 빼고 써보세요. 보통 이게 더 안전합니다.
-import './index.css' 
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
