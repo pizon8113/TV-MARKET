7916eea7f70d96ec38d694cfff0528919caa3b66
pizon8113 <pizon8113@gmail.com>
2026-03-16 23:43:55 +0900

    Fix: correct main.jsx import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // CSS 파일이 있다면 유지, 없다면 이 줄을 지워주세요.
+import App from './App' // .jsx를 빼고 써보세요. 보통 이게 더 안전합니다.
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
